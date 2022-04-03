3d6c7216646cbab68bd275b0d3b9fdce4a972554
Kavya Matcha <61905658+Kavya-2000@users.noreply.github.com>
2022-04-03 14:33:04 +0530

    Update function.c

--- 3_Implementation/src/function.c
+++ 3_Implementation/src/function.c
@@ -1,13 +1,4 @@
-/**
- * @file function.c
- * @author raaga.vardhini@ltts.com
- * @brief 
- * @version 0.1
- * @date 2021-09-06
- * 
- * @copyright Copyright (c) 2021
- * 
- */
+
 #include "main.h"
 void flush()
 {
