f162c830c5c12057a3a7658dc45e063bbc1afc7e
Kavya Matcha <61905658+Kavya-2000@users.noreply.github.com>
2022-04-03 11:49:01 +0530

    Update test_employee.c

--- 3_Implementation/test/test_employee.c
+++ 3_Implementation/test/test_employee.c
@@ -33,15 +33,15 @@ int main()
 
 /* Write all the test functions */ 
 void test_ADD_Record(void) {
-  TEST_ASSERT_EQUAL(raaga 22 35000, Add_Record(raaga, 22, 35000));
+  TEST_ASSERT_EQUAL(kavya 22 35000, Add_Record(kavya, 22, 35000));
   
   /* Dummy fail*/
-  TEST_ASSERT_EQUAL(12 raaga 35000, Add_Record(raaga, 12, 7500));
+  TEST_ASSERT_EQUAL(12 kavya 35000, Add_Record(kavya, 12, 7500));
 }
 
 void test_Modify_Record(void) {
-  TEST_ASSERT_EQUAL(raaga, Modify_Record( raaga));
+  TEST_ASSERT_EQUAL(kavya, Modify_Record( kavya));
   
   /* Dummy fail*/
-  TEST_ASSERT_EQUAL(raaga, Modify_Record(abc));
+  TEST_ASSERT_EQUAL(kavya, Modify_Record(abc));
 }
